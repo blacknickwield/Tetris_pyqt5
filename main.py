466bf2ac0db9226520af498438fa99639e1a0880
buaazyc <yichuan@buaa.edu.cn>
2019-10-03 10:58:15 +0800

    首次提交

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ author：张艺川
 last edit time：2019年10月3号
 
 参考：http://code.py40.com/2052.html
-代码包括四个类：Tetris, Board, Tetrominoe, Shape
-
-Tetris类存放游戏
-Board编写游戏逻辑
-Tetrominoe类包含所有俄罗斯名字
-Shape包含俄罗斯方块的代码
 
 在比赛开始后立即启动
 我们可以通过按p键暂停游戏
-空格键将立即把俄罗斯方块块底部
+左右键移动方块
+上下键旋转方块
+D键加速下落
+空格键立即将方块下落至底部
+分数是已经删除的行数，显示在左下角
 
-分数是已经删除的行数
+Tetrominoe.py定义8种方块的索引，用0-7表示
+Shape.py使用Terominoe.py中的索引，封装了方块的坐标和形状，以及相应的方法
+Board.py实现了游戏主要的逻辑
+Tetris.py使用pyqt5定义游戏界面、窗口
+main.py是游戏的入口，直接运行即可开始游戏
+
 """
 import sys
 from PyQt5.QtWidgets import QApplication
